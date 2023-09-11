c2bb10aa00b74cf44b11488e1d4093acf6c9e757
kmseunh <tmdgus8779@gmail.com>
2023-09-12 00:00:28 +0900

    feat: Add a Button
    Add a Delete Button

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -2,14 +2,29 @@ import { useContext } from 'react';
 import { AppContext } from '../contexts/AppContext';
 
 const Favorites = () => {
-    const { favorites } = useContext(AppContext);
+    const { favorites, setFavorites } = useContext(AppContext);
+
+    // favorite 삭제 함수
+    const onDeleteFavorite = (index) => {
+        const updatedFavorites = [...favorites];
+        updatedFavorites.splice(index, 1);
+        setFavorites(updatedFavorites);
+    };
 
     return (
         <div>
             <h2>Favorites</h2>
             <ul>
                 {favorites.map((favorite, index) => (
-                    <li key={index}>{favorite}</li>
+                    <li key={index}>
+                        {favorite}
+                        <button
+                            onClick={() => onDeleteFavorite(index)}
+                            className='ml-2 text-red-500'
+                        >
+                            Delete
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
