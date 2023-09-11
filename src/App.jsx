ff0ea5c09e93526e4cf99837789631e43958503b
kmseunh <tmdgus8779@gmail.com>
2023-09-11 23:16:12 +0900

    feat: Add route and component for home page
    Add a new route for the home page in the 'Routes' component
    Import 'AppProvider' and 'Home' component
    Set up the route to render the 'Home' component when the root path is accessed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Route, Routes } from 'react-router-dom';
+import AppProvider from './contexts/AppContext';
+import Home from './pages/Home';
 
-function App() {
-    const [count, setCount] = useState(0);
-
+const App = () => {
     return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank' rel='noreferrer'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1 className='font-bold'>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <AppProvider>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </AppProvider>
     );
-}
+};
 
 export default App;
